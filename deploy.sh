22500926f7580fee7b7852fbedad22ebe3a745ca
Hiroyuki Onaka <hiroyuki@fieldnotes.jp>
2017-05-04 21:07:21 +0900

    デプロイスクリプト修正

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,4 @@ fi
 TARGET="sweetmusic-vol3"
 
 curl -X POST -f -H "Authorization: Bearer ${DROPBOX_TOKEN}" \
-	-D - -H "Dropbox-API-Arg: {\"path\": \"/techbookfest2${TIMESTAMP}.pdf\",\"mode\": \"overwrite\",\"mute\": false}" -H "Content-Type: application/octet-stream" --data-binary @target/${TARGET}.pdf https://content.dropboxapi.com/2/files/upload
+	-D - -H "Dropbox-API-Arg: {\"path\": \"/${TARGET}${TIMESTAMP}.pdf\",\"mode\": \"overwrite\",\"mute\": false}" -H "Content-Type: application/octet-stream" --data-binary @target/${TARGET}.pdf https://content.dropboxapi.com/2/files/upload
