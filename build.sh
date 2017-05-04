861769729523cfc70c4182b09370754fd8455fe0
Hiroyuki Onaka <hiroyuki@fieldnotes.jp>
2017-05-04 21:41:17 +0900

    ビルドスクリプト修正

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 set -eux
 
+export LANG="ja_JP.UTF-8"
+
 rm -rf target
 mkdir -p target
 
@@ -14,5 +16,5 @@ cat src/*.yaml >> target/tmp.md
 
 cat -n target/tmp.md
 
-pandoc -V fontsize:14pt -V papersize:b5 -f markdown+raw_tex+tex_math_dollars+citations+yaml_metadata_block --filter pandoc-citeproc -o target/${OUTPUT}.pdf --latex-engine=lualatex -H h-luatexja.tex -A src/imprint.tex -B b-fancy.tex  --toc --toc-depth=1 target/tmp.md
+pandoc -V fontsize:14pt -V papersize:b5 -s -f markdown+raw_tex+tex_math_dollars+citations+yaml_metadata_block --filter pandoc-citeproc --filter pandoc-crossref -M "crossrefYaml=crossref_config.yaml" -o target/${OUTPUT}.pdf --latex-engine=lualatex -H h-luatexja.tex -A src/imprint.tex -B b-fancy.tex  --toc --toc-depth=1 target/tmp.md
 
