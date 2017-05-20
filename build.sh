a4cffecee6294cb92c020ca2abc2b1dc3264e821
Hiroyuki Onaka <hiroyuki@fieldnotes.jp>
2017-05-21 01:38:14 +0900

    WIP

--- build.sh
+++ build.sh
@@ -16,7 +16,8 @@ cat src/*.yaml >> target/tmp.md
 
 cat -n target/tmp.md
 
-pandoc -V fontsize:14pt -V papersize:b5 -s -f markdown+raw_tex+tex_math_dollars+citations+yaml_metadata_block --filter pandoc-citeproc --filter pandoc-crossref -M "crossrefYaml=crossref_config.yaml" -o target/sweetmusic-vol3.pdf --latex-engine=lualatex -H h-luatexja.tex -A src/imprint.tex -B b-fancy.tex  --toc --toc-depth=1 target/tmp.md --verbose
+#pandoc -V fontsize:14pt -V papersize:b5 -s -f markdown+raw_tex+tex_math_dollars+citations+yaml_metadata_block --filter pandoc-citeproc --filter pandoc-crossref -M "crossrefYaml=crossref_config.yaml" -o target/sweetmusic-vol3.pdf --latex-engine=lualatex -H h-luatexja.tex -A src/imprint.tex -B b-fancy.tex  --toc --toc-depth=1 target/tmp.md --verbose
+pandoc -V fontsize:14pt -V papersize:b5 -s -f markdown+raw_tex+tex_math_dollars+citations+yaml_metadata_block --filter pandoc-crossref -M "crossrefYaml=crossref_config.yaml" --filter pandoc-citeproc -o target/sweetmusic-vol3.pdf --latex-engine=lualatex -H h-luatexja.tex -A src/imprint.tex -B b-fancy.tex  --toc --toc-depth=1 target/tmp.md --verbose
 
 
 
