ce3c562bc571eb5a459a0c90012c218b0963b490
Hiroyuki Onaka <hiroyuki@fidldnotes.jp>
2017-06-09 20:12:10 +0900

    WIP.

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@ set -eux
 
 export LANG="ja_JP.UTF-8"
 
+cat /usr/local/texlive/2016/texmf-dist/tex/latex/listings/listings.sty
+
 rm -rf target
 mkdir -p target
 
@@ -17,7 +19,7 @@ cat src/*.yaml >> target/tmp.md
 cat -n target/tmp.md
 
 #pandoc -V fontsize:14pt -V papersize:b5 -s -f markdown+raw_tex+tex_math_dollars+citations+yaml_metadata_block --filter pandoc-citeproc --filter pandoc-crossref -M "crossrefYaml=crossref_config.yaml" -o target/sweetmusic-vol3.pdf --latex-engine=lualatex -H h-luatexja.tex -A src/imprint.tex -B b-fancy.tex  --toc --toc-depth=1 target/tmp.md --verbose
+#pandoc -V fontsize:14pt -V papersize:b5 -s -f markdown+raw_tex+tex_math_dollars+citations+yaml_metadata_block --filter pandoc-crossref -M "crossrefYaml=crossref_config.yaml" --filter pandoc-citeproc -o target/sweetmusic-vol3.pdf --latex-engine=lualatex -H h-luatexja.tex -A src/imprint.tex -B b-fancy.tex  -B ha-luatexja.tex --toc --toc-depth=1 --listings target/tmp.md --verbose
+
 pandoc -V fontsize:14pt -V papersize:b5 -s -f markdown+raw_tex+tex_math_dollars+citations+yaml_metadata_block --filter pandoc-crossref -M "crossrefYaml=crossref_config.yaml" --filter pandoc-citeproc -o target/sweetmusic-vol3.pdf --latex-engine=lualatex -H h-luatexja.tex -A src/imprint.tex -B b-fancy.tex  -B ha-luatexja.tex --toc --toc-depth=1 --listings target/tmp.md --verbose
-
-
 
